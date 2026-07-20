cc28b4cb2505174a16be073f2de709f93002dbca
Cedric Bouhadra <cebouhad@c4b5c6.42berlin.de>
2026-07-20 09:57:00 +0200

    thread_test

--- time.c
+++ time.c
@@ -6,7 +6,7 @@
 /*   By: cebouhad <cebouhad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/17 12:20:06 by cdric.b           #+#    #+#             */
-/*   Updated: 2026/07/20 08:43:40 by cebouhad         ###   ########.fr       */
+/*   Updated: 2026/07/20 09:56:40 by cebouhad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,41 +50,67 @@ time_t time_diff(time_t start, time_t end)
     
     return elapsed;
 }
-
+struct s_mu
+{
+    pthread_mutex_t *mu_memset;
+    pthread_mutex_t *mu_printf;
+};
 
 
 void *thread(void *data)
 {
     int i;
     pthread_t id;
+    struct s_mu *mu; 
     struct timespec start;
     struct timespec end;
     
+    id = pthread_self();
+    
     i = 10;
+    mu = (struct s_mu *)data;
+    printf("Start thread %p\n", &(mu->mu_memset));
+    
+    pthread_mutex_lock(mu->mu_memset);
+    printf("resultat du lock\n");
     memset(&start, 0, sizeof(struct timespec));
     memset(&end, 0, sizeof(struct timespec));
-    id = pthread_self();
+    pthread_mutex_unlock(mu->mu_memset);
+    printf("resultat du unlock\n");
     while (i >= 0)
     {
         clock_gettime(CLOCK_MONOTONIC, &start);
         usleep(200000);
         clock_gettime(CLOCK_MONOTONIC, &end);
+        pthread_mutex_lock(mu->mu_printf);
         printf("[thread %ld]elapsed time %ld\n", id,  time_diff(start.tv_nsec, end.tv_nsec));
+        pthread_mutex_unlock(mu->mu_printf);
         i--;
     }
     return (NULL);
 }
 
+
 int main(void)
 {
     pthread_t t1;
     pthread_t t2;
-    
+    pthread_mutex_t m1;
+    pthread_mutex_t m2;
+    struct s_mu *mu;
 
-    pthread_create(&t1, NULL, thread, NULL);
-    pthread_create(&t2, NULL, thread, NULL);
+    mu = malloc(sizeof(struct s_mu));
+    assert(mu);
+    assert(pthread_mutex_init(&m1, NULL) == 0);
+    assert(pthread_mutex_init(&m2, NULL) == 0);
+    mu->mu_memset = &m1;
+    mu->mu_printf = &m2;
+    pthread_create(&t1, NULL, thread, mu);
+    //pthread_create(&t2, NULL, thread, &mu);
     pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
+    //pthread_join(t2, NULL);
+    pthread_mutex_destroy(&m1);
+    pthread_mutex_destroy(&m2);
 
     return (0);
 }
