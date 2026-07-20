2f2dc6c89655fc07a10ba8766e44a3c8d3713dc0
Cedric Bouhadra <cebouhad@c4b5c6.42berlin.de>
2026-07-20 22:53:48 +0200

    mutex.c norminette + clean memory

--- srcs/mutex.c
+++ srcs/mutex.c
@@ -6,79 +6,81 @@
 /*   By: cebouhad <cebouhad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/18 21:21:34 by cebouhad          #+#    #+#             */
-/*   Updated: 2026/07/20 22:49:10 by cebouhad         ###   ########.fr       */
+/*   Updated: 2026/07/20 22:53:17 by cebouhad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/codexion.h"
 
-void *destroy_arr_mutex(t_mutex **mu, size_t idx)
+static void	*destroy_arr_mutex(t_mutex **mu, size_t idx)
 {
-    size_t i;
+	size_t	i;
 
-    i = 0;
-    while (i < idx)
-        pthread_mutex_destroy(&(*mu)[i++]);
-    free(*mu);
-    return (NULL);
+	i = 0;
+	while (i < idx)
+		pthread_mutex_destroy(&(*mu)[i++]);
+	free(*mu);
+	return (NULL);
 }
 
-int clean_gmutex(t_global_mutex *mu, size_t coders)
+static t_mutex	*get_arr_of_initialised_mutex(size_t coders)
 {
-    size_t i;
+	size_t	i;
+	t_mutex	*arr;
 
-    pthread_mutex_destroy(&mu->display_f);
-    pthread_mutex_destroy(&mu->timestamp_f);
-    if (mu->dongles)
-        destroy_arr_mutex(&mu->dongles, coders);
-    if (mu->timestamp_data)
-        destroy_arr_mutex(&mu->timestamp_data, coders);
-    return (FALSE);
+	arr = malloc(sizeof(t_mutex) * coders);
+	if (!arr)
+		return (NULL);
+	i = 0;
+	while (i < coders)
+	{
+		if (pthread_mutex_init(&arr[i], NULL))
+			return (destroy_arr_mutex(&arr, i));
+		i++;
+	}
+	return (arr);
 }
 
-t_mutex *get_arr_of_initialised_mutex(size_t coders)
+static int	mutex_initialisation(t_mutex *mu)
 {
-    t_mutex *arr;
-    size_t  i;
-
-    arr = malloc(sizeof(t_mutex) * coders);
-    if (!arr)
-        return (NULL);
-    i = 0;
-    while (i < coders)
-    {
-        if (pthread_mutex_init(&arr[i], NULL))
-            return (destroy_arr_mutex(&arr, i));
-        i++;
-    }
-    return (arr);
+	if (pthread_mutex_init(mu, NULL))
+		return (FALSE);
+	return (TRUE);
 }
 
-int mutex_initialisation(t_mutex *mu)
+int	clean_gmutex(t_global_mutex *mu, size_t coders)
 {
-    if(pthread_mutex_init(mu, NULL))
-        return (FALSE);
-    return (TRUE);
+	size_t	i;
+
+	pthread_mutex_destroy(&mu->display_f);
+	pthread_mutex_destroy(&mu->timestamp_f);
+	if (mu->dongles)
+		destroy_arr_mutex(&mu->dongles, coders);
+	if (mu->timestamp_data)
+		destroy_arr_mutex(&mu->timestamp_data, coders);
+	return (FALSE);
 }
 
-int g_mutex_initialisation(t_global_mutex  *gmutex, size_t coders)
+int	g_mutex_initialisation(t_global_mutex *gmutex, size_t coders)
 {
-    if(!mutex_initialisation(&gmutex->display_f))
-        return (clean_gmutex(gmutex, coders));
-    if(!mutex_initialisation(&gmutex->timestamp_f))
-        return (clean_gmutex(gmutex, coders));
-    gmutex->dongles = get_arr_of_initialised_mutex(coders);
-    if(!gmutex->dongles)
-    {
-        write(STDERR_FILENO, "Error mutex dongle initialisation\n", strlen("Error mutex dongle initialisation\n"));
-        return (clean_gmutex(gmutex, coders));
-    }
-    gmutex->timestamp_data = NULL;
-    gmutex->timestamp_data = get_arr_of_initialised_mutex(coders);
-    if(!gmutex->timestamp_data)
-    {
-        write(STDERR_FILENO, "Error mutex dongle initialisation\n", strlen("Error mutex dongle initialisation\n"));
-        return (clean_gmutex(gmutex, coders));
-    }
-    return (TRUE);
-}   
+	if (!mutex_initialisation(&gmutex->display_f))
+		return (clean_gmutex(gmutex, coders));
+	if (!mutex_initialisation(&gmutex->timestamp_f))
+		return (clean_gmutex(gmutex, coders));
+	gmutex->dongles = get_arr_of_initialised_mutex(coders);
+	if (!gmutex->dongles)
+	{
+		write(STDERR_FILENO, "Error mutex dongle initialisation\n", \
+			strlen("Error mutex dongle initialisation\n"));
+		return (clean_gmutex(gmutex, coders));
+	}
+	gmutex->timestamp_data = NULL;
+	gmutex->timestamp_data = get_arr_of_initialised_mutex(coders);
+	if (!gmutex->timestamp_data)
+	{
+		write(STDERR_FILENO, "Error mutex dongle initialisation\n", \
+			strlen("Error mutex dongle initialisation\n"));
+		return (clean_gmutex(gmutex, coders));
+	}
+	return (TRUE);
+}
